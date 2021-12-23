24a9af5f708e83964a287d9979da6f84eb4ba043
unknown <you@example.com>
2021-12-23 15:46:46 +0900

    fixing core error

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ const authRouter = require("./routes/auth");
 const postRouter = require("./routes/post");
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(authRouter);
 app.use(postRouter);
